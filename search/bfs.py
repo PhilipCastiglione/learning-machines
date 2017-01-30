acd74771b581555204f534c9ed032092e94a0e78
Philip Castiglione <philipcastiglione@gmail.com>
2017-01-30 16:50:53 +1100

    finished bfs

--- search/bfs.py
+++ search/bfs.py
@@ -4,36 +4,68 @@ class Node:
     def __init__(self, state, parent):
         self.state = state
         self.parent = parent
-        self.on_frontier = False
-        self.explored = False
+
+class Queue:
+    def __init__(self):
+        self.contents = []
+        self.max = 0
+
+    def enqueue(self, element):
+        self.contents.insert(0, element)
+        if len(self.contents) > self.max:
+            self.max = len(self.contents)
+
+    def dequeue(self):
+        return self.contents.pop()
 
 class Bfs:
     def __init__(self, state_machine):
         self.state_machine = state_machine
-        self.frontier = []
+        self.frontier = Queue()
+        self.explored = []
+        self.success_node = None
 
     def success(self):
         return self.state_machine.current_state() == self.state_machine.goal_state()
 
+    def visited_nodes(self):
+        return self.explored + self.frontier.contents or []
+
+    def add_next_states(self, parent):
+        for state in self.state_machine.next_states():
+            if [node.state for node in self.visited_nodes()].count(state) == 0:
+                self.frontier.enqueue(Node(state, parent))
+
     def search(self):
-        if self.success():
-            # TODO: starting in winning position
-            return True
-
-        initial_state = Node(self.state_machine.current_state(), None)
-        initial_state.explored = True
-
-        self.frontier = initial_state.next_states()
+        self.frontier.enqueue(Node(self.state_machine.current_state(), None))
 
         while self.frontier:
-            pass
+            node = self.frontier.dequeue()
+            self.explored.append(node)
 
-        # bail when we get a success
-        # while the frontier isn't empty...
-        # explore according to breath
-        # if the frontier is empty and we never got success
-        # then bail because we failed
-        pass
+            self.state_machine.set_state(node.state)
+
+            if self.success():
+                self.success_node = node
+                return True
+
+            self.add_next_states(node)
+
+        return False
 
     def results(self):
-        pass
+        moves = [self.success_node]
+        parent = self.success_node.parent
+        while parent:
+            moves.append(parent)
+            parent = parent.parent
+        moves.reverse()
+
+        return {
+            "cost": len(moves),
+            "path": [node.state for node in moves],
+            "visited_nodes": len(self.visited_nodes()),
+            "frontier_nodes": len(self.frontier.contents),
+            "max_frontier_nodes": self.frontier.max,
+            "max_search_depth": len(moves)
+        }
