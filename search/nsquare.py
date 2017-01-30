3cd1ee9ff4883b65bd4cedf515c787ddeaf01109
Philip Castiglione <philipcastiglione@gmail.com>
2017-01-30 17:35:58 +1100

    documented nsquare board

--- search/nsquare.py
+++ search/nsquare.py
@@ -1,23 +1,46 @@
-# TODO: docs
 import math
 
 class Board:
+    """Board defines the shape, contents and rules for the n-square puzzle
+    board, and makes itself available as a state machine for search algorithms.
+    """
+
+    """Instantiate with an initial state and cache dimensions."""
     def __init__(self, initial_state):
         self.dim = int(math.sqrt(len(initial_state) + 1))
-        self.tiles = []
-        for num in initial_state:
-            self.tiles.append(num)
+        self.tiles = initial_state
 
+    """For debugging."""
     def print(self):
         for i in range(self.dim):
             for j in range(self.dim):
                 print("{:2} ".format(self.tiles[i * self.dim + j]), end=" ")
             print()
 
+    """Returns the current state."""
     def current_state(self):
         return self.tiles
 
-    def moves(self):
+    """Returns the legal states available from the current state."""
+    def next_states(self):
+        states = []
+        for move in self._moves():
+            states.append(self._state_for_move(move))
+        return states
+
+    """Sets the current state."""
+    def set_state(self, state):
+        self.tiles = state
+
+    """Returns the goal state."""
+    def goal_state(self):
+        state = list(range(len(self.tiles)))
+        state.remove(0)
+        state.append(0)
+        return state
+
+    """Calculate and return the indices of legal moves."""
+    def _moves(self):
         moves = []
         empty = self.tiles.index(0)
         north = empty - self.dim
@@ -34,23 +57,9 @@ class Board:
             moves.append(west)
         return moves
 
-    def state_for_move(self, tile_idx):
+    """Calculate and return the state associated with a legal move."""
+    def _state_for_move(self, tile_idx):
         tiles = self.tiles.copy()
         tiles[self.tiles.index(0)] = tiles[tile_idx]
         tiles[tile_idx] = 0
         return tiles
-
-    def next_states(self):
-        states = []
-        for move in self.moves():
-            states.append(self.state_for_move(move))
-        return states
-
-    def set_state(self, state):
-        self.tiles = state
-
-    def goal_state(self):
-        state = list(range(len(self.tiles)))
-        state.remove(0)
-        state.append(0)
-        return state
