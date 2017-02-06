e8d23feab49d8b88e68e634e53c78937b211cd13
Philip Castiglione <philipcastiglione@gmail.com>
2017-02-06 17:38:34 +1100

    cities.py

--- search/cities.py
+++ search/cities.py
@@ -1,7 +1,7 @@
 {
-  "San Francisco": { "Los Angeles": 100, "Salt Lake City": 156, "Portland": 151 },
-  "Portland": {},
-  "Seattle": {},
+  "San Francisco": {"Los Angeles": 100, "Salt Lake City": 156, "Portland": 151},
+  "Portland": {"San Francisco": 151, "Salt Lake City": 175, "Seattle": 44},
+  "Seattle": {"Portland": 44, "Vancouver": 45, "Calgary": 118, "Helena": 189},
   "Vancouver": {},
   "Los Angeles": {},
   "Las Vegas": {},
