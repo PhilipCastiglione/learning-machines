6c0bffcc6a7bc5b27de3ced3cc01fc004d3c9f6e
Philip Castiglione <philipcastiglione@gmail.com>
2017-02-07 17:02:48 +1100

    fixed ucs to accumulate cost

--- search/algorithms/ucs.py
+++ search/algorithms/ucs.py
@@ -1,5 +1,10 @@
+"""Uniform Cost Search is a search strategy where, starting at the root of a
+tree of nodes to explore, each node's children are explored in order of lowest
+movement cost, with cost accumulating per move and priority given to nodes
+remaining with least cost.
+"""
+
 # TODO: docs
-# TODO: test
 
 class Node:
     def __init__(self, state, parent, cost):
@@ -17,10 +22,11 @@ class Heap:
         if len(self.contents) > self.max:
             self.max = len(self.contents)
 
-    def update_costs(self, state, cost):
+    def update_costs(self, state, parent, cost):
         for node in self.contents:
             if node.state == state and node.lowest_cost > cost:
                 node.lowest_cost = cost
+                node.parent = parent
 
     def extract_min(self):
         values = [node.lowest_cost for node in self.contents]
@@ -33,6 +39,7 @@ class Ucs:
         self.frontier = Heap()
         self.explored = []
         self.success_node = None
+        self.current_cost = 0
 
     def search(self):
         self.frontier.insert(Node(self.subject.current_state(), None, 0))
@@ -40,6 +47,8 @@ class Ucs:
         while self.frontier.contents:
             node = self.frontier.extract_min()
             self.explored.append(node)
+
+            self.current_cost = node.lowest_cost
 
             self.subject.set_state(node.state)
 
@@ -65,7 +74,7 @@ class Ucs:
             moves = []
 
         return {
-            "cost": len(moves),
+            "cost": self.current_cost,
             "path": [node.state for node in moves],
             "visited_nodes": len(self._visited_nodes()),
             "frontier_nodes": len(self.frontier.contents),
@@ -78,9 +87,10 @@ class Ucs:
 
     def _add_next_states(self, parent):
         for state in self.subject.next_states():
+            cost = self.current_cost
             if [node.state for node in self._visited_nodes()].count(state) == 0:
-                cost = self.subject.move_cost(self.subject.current_state(), state)
+                cost += self.subject.move_cost(self.subject.current_state(), state)
                 self.frontier.insert(Node(state, parent, cost))
             elif [node.state for node in self.frontier.contents].count(state) == 1:
-                cost = self.subject.move_cost(self.subject.current_state(), state)
-                self.frontier.update_costs(state, cost)
+                cost += self.subject.move_cost(self.subject.current_state(), state)
+                self.frontier.update_costs(state, parent, cost)
