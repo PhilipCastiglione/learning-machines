aa7b3779a49799424dd85a32b883d2daf0f0f1c8
Philip Castiglione <philipcastiglione@gmail.com>
2017-02-07 16:05:21 +1100

    fixed bugs in ucs and added results

--- search/algorithms/ucs.py
+++ search/algorithms/ucs.py
@@ -23,7 +23,7 @@ class Heap:
                 node.lowest_cost = cost
 
     def extract_min(self):
-        values = [node.lowest_cost for nodes in self.contents]
+        values = [node.lowest_cost for node in self.contents]
         idx = values.index(min(values))
         return self.contents.pop(idx)
 
@@ -35,7 +35,7 @@ class Ucs:
         self.success_node = None
 
     def search(self):
-        self.frontier.insert(Node(self.subject.current_state(), None))
+        self.frontier.insert(Node(self.subject.current_state(), None, 0))
 
         while self.frontier.contents:
             node = self.frontier.extract_min()
@@ -51,8 +51,27 @@ class Ucs:
 
         return False
 
+    """Returns statistics describing the search."""
+    # TODO: update results output and document
     def results(self):
-        pass # TODO
+        if self.success_node:
+            moves = [self.success_node]
+            parent = self.success_node.parent
+            while parent:
+                moves.append(parent)
+                parent = parent.parent
+            moves.reverse()
+        else:
+            moves = []
+
+        return {
+            "cost": len(moves),
+            "path": [node.state for node in moves],
+            "visited_nodes": len(self._visited_nodes()),
+            "frontier_nodes": len(self.frontier.contents),
+            "max_frontier_nodes": self.frontier.max,
+            "max_search_depth": len(moves)
+        }
 
     def _visited_nodes(self):
         return self.explored + self.frontier.contents or []
