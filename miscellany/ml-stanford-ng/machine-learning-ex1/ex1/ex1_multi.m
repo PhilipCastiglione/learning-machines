59679286dd52d82d64324300662cac2db6d95ed0
Philip Castiglione <philipcastiglione@gmail.com>
2017-11-10 15:22:18 +1100

    price predictions

--- miscellany/ml-stanford-ng/machine-learning-ex1/ex1/ex1_multi.m
+++ miscellany/ml-stanford-ng/machine-learning-ex1/ex1/ex1_multi.m
@@ -131,8 +131,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+house_features = [1650 3]
+house_norm = (house_features - mu)./ sigma;
+price = [1 house_norm] * theta;
 
 % ============================================================
 
@@ -176,8 +177,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+price = [1 house_features] * theta;
 
 % ============================================================
 
