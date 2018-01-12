64819f5bb76e3d923d72257b8d9d88ef15d9dcf2
Thomas Castiglione <castiglione@mac.com>
2018-01-12 22:06:25 +0800

    break down the costs further

--- miscellany/riddle-competitions/golad/lib/bench.py
+++ miscellany/riddle-competitions/golad/lib/bench.py
@@ -9,7 +9,7 @@ def bench(name, f):
     before = time.perf_counter()
     f()
     after = time.perf_counter()
-    elapsed = (after - before) * 1000
+    elapsed = int((after - before) * 1000)
     print(f'{name}: {elapsed}ms')
 
 if __name__ == '__main__':
@@ -29,12 +29,20 @@ if __name__ == '__main__':
     game = Game()
 
     # mock an update to setup state
-    game._handle_update("update game round 0")
-    game._handle_update("update game field .,.,.,1,.,0,.,.,.,.,.,0,.,1,.,.,1,.,.,.,.,.,.,.,0,.,.,1,1,.,.,.,1,1,.,.,0,.,.,.,0,.,.,0,1,0,1,.,.,.,1,.,.,.,1,.,.,.,.,1,.,0,.,.,1,.,0,.,1,.,1,1,.,1,.,1,.,.,0,1,.,0,.,0,.,0,.,.,.,1,1,.,.,.,.,0,.,.,1,.,.,.,.,.,0,.,0,1,0,.,.,.,.,.,.,1,.,1,0,.,1,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,.,0,1,0,1,.,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,0,.,1,0,.,0,.,.,.,.,.,.,1,0,1,.,1,.,.,.,.,.,0,.,.,1,.,.,.,.,0,0,.,.,.,1,.,1,.,1,.,0,1,.,.,0,.,0,.,0,0,.,0,.,1,.,0,.,.,1,.,0,.,.,.,.,0,.,.,.,0,.,.,.,0,1,0,1,.,.,1,.,.,.,1,.,.,0,0,.,.,.,0,0,.,.,1,.,.,.,.,.,.,.,0,.,.,0,.,1,.,.,.,.,.,1,.,0,.,.,.")
-    game._handle_update("update player0 living_cells 50")
-    game._handle_update("update player1 living_cells 50")
+    def handle_update():
+        game._handle_update("update game round 0")
+        game._handle_update("update game field .,.,.,1,.,0,.,.,.,.,.,0,.,1,.,.,1,.,.,.,.,.,.,.,0,.,.,1,1,.,.,.,1,1,.,.,0,.,.,.,0,.,.,0,1,0,1,.,.,.,1,.,.,.,1,.,.,.,.,1,.,0,.,.,1,.,0,.,1,.,1,1,.,1,.,1,.,.,0,1,.,0,.,0,.,0,.,.,.,1,1,.,.,.,.,0,.,.,1,.,.,.,.,.,0,.,0,1,0,.,.,.,.,.,.,1,.,1,0,.,1,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,.,0,1,0,1,.,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,0,.,1,0,.,0,.,.,.,.,.,.,1,0,1,.,1,.,.,.,.,.,0,.,.,1,.,.,.,.,0,0,.,.,.,1,.,1,.,1,.,0,1,.,.,0,.,0,.,0,0,.,0,.,1,.,0,.,.,1,.,0,.,.,.,.,0,.,.,.,0,.,.,.,0,1,0,1,.,.,1,.,.,.,1,.,.,0,0,.,.,.,0,0,.,.,1,.,.,.,.,.,.,.,0,.,.,0,.,1,.,.,.,.,.,1,.,0,.,.,.")
+        game._handle_update("update player0 living_cells 50")
+        game._handle_update("update player1 living_cells 50")    
+    bench('handle_update', handle_update)
 
-    # run one move
-    bench('build_children', game.current_node.build_children)
-    bench('move', game.move)
+    # broken down contents of build_children()
+    bench('_build_pass', game.current_node._build_pass)
+    bench('_build_kill', game.current_node._build_kill)
+    bench('_filter_best_kill_moves', game.current_node._filter_best_kill_moves)
+    bench('_build_birth', game.current_node._build_birth)
+    bench('_update_minimax', game.current_node._update_minimax)
+
+    # output move
+    # bench('move', game.move)
 
