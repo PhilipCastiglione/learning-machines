11ad66a32a8eb2e095403bc6230634c446e0cfc3
Thomas Castiglione <castiglione@mac.com>
2018-01-12 21:49:40 +0800

    create simple test harness to run one move

---
+++ miscellany/riddle-competitions/golad/lib/bench.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+from game import Game
+from settings import Settings
+
+if __name__ == '__main__':
+    # mock settings
+    settings = {}
+    Settings._add_setting(settings, 'settings timebank 10000')
+    Settings._add_setting(settings, 'settings time_per_move 100')
+    Settings._add_setting(settings, 'settings player_names player0,player1')
+    Settings._add_setting(settings, 'settings your_bot player0')
+    Settings._add_setting(settings, 'settings your_botid 0')
+    Settings._add_setting(settings, 'settings field_width 18')
+    Settings._add_setting(settings, 'settings field_height 16')
+    Settings._add_setting(settings, 'settings max_rounds 100')
+    Settings._load_settings(settings)
+
+    # create engine
+    game = Game()
+
+    # mock an update to setup state
+    game._handle_update("update game round 0")
+    game._handle_update("update game field .,.,.,1,.,0,.,.,.,.,.,0,.,1,.,.,1,.,.,.,.,.,.,.,0,.,.,1,1,.,.,.,1,1,.,.,0,.,.,.,0,.,.,0,1,0,1,.,.,.,1,.,.,.,1,.,.,.,.,1,.,0,.,.,1,.,0,.,1,.,1,1,.,1,.,1,.,.,0,1,.,0,.,0,.,0,.,.,.,1,1,.,.,.,.,0,.,.,1,.,.,.,.,.,0,.,0,1,0,.,.,.,.,.,.,1,.,1,0,.,1,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,.,0,1,0,1,.,0,.,.,.,.,.,0,.,.,.,.,.,.,1,.,.,.,.,.,1,0,.,1,0,.,0,.,.,.,.,.,.,1,0,1,.,1,.,.,.,.,.,0,.,.,1,.,.,.,.,0,0,.,.,.,1,.,1,.,1,.,0,1,.,.,0,.,0,.,0,0,.,0,.,1,.,0,.,.,1,.,0,.,.,.,.,0,.,.,.,0,.,.,.,0,1,0,1,.,.,1,.,.,.,1,.,.,0,0,.,.,.,0,0,.,.,1,.,.,.,.,.,.,.,0,.,.,0,.,1,.,.,.,.,.,1,.,0,.,.,.")
+    game._handle_update("update player0 living_cells 50")
+    game._handle_update("update player1 living_cells 50")
+
+    # run one move
+    game.current_node.build_children()
+    game.move()
