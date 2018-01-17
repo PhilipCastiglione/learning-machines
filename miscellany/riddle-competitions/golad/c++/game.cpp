5399dd02c508140b6dedb8dc2d07459c0c7e6143
Philip Castiglione <philipcastiglione@gmail.com>
2018-01-17 22:46:07 +1100

    fixed bugs in game logic, implemented addBirthNodes

--- miscellany/riddle-competitions/golad/c++/game.cpp
+++ miscellany/riddle-competitions/golad/c++/game.cpp
@@ -35,6 +35,7 @@ void processAction()
   if (type == "move") {
     timebank = stoi(time);
     makeMove();
+    cout << "pass\n"; // TODO: remove
   } else
     cerr << "action type: " << type << " not expected\n";
 }
@@ -47,19 +48,19 @@ void processUpdate()
   token >> value;
 
   if (target == "game") {
-    if (field == "round")
+    if (field == "round") {
       round = stoi(value);
-    if (field == "field")
+    }
+    if (field == "field") {
       parseState(value);
-  }
-  if (target == "player0" or target == "player1") {
+    }
+  } else if (target == "player0" or target == "player1") {
     if (field == "living_cells") {
       if (target[6] == botId)
         myLiveCells = stoi(value);
       else
         theirLiveCells = stoi(value);
-    }
-    if (field != "node")
+    } else if (field != "move")
       cerr << "update playerX field: " << field << " not expected\n";
   } else
     cerr << "update target: " << target << " not expected\n";
@@ -74,34 +75,27 @@ void processSettings()
   if (field == "player_names") {
     if (value != "player0,player1")
       cerr << "settings player_names value: " << value << " not expected\n";
-  }
-  if (field == "your_bot") {
+  } else if (field == "your_bot") {
     if (value != "player0" and value != "player1")
       cerr << "settings your_bot value: " << value << " not expected\n";
-  }
-  if (field == "timebank") {
+  } else if (field == "timebank") {
     if (value == "10000")
       timebank = stoi(value);
     else
       cerr << "settings timebank value: " << value << " not expected\n";
-  }
-  if (field == "time_per_move") {
+  } else if (field == "time_per_move") {
     if (value != "100")
       cerr << "settings time_per_move value: " << value << " not expected\n";
-  }
-  if (field == "field_width") {
+  } else if (field == "field_width") {
     if (value != "18")
       cerr << "settings field_width value: " << value << " not expected\n";
-  }
-  if (field == "field_height") {
+  } else if (field == "field_height") {
     if (value != "16")
       cerr << "settings field_height value: " << value << " not expected\n";
-  }
-  if (field == "max_rounds") {
+  } else if (field == "max_rounds") {
     if (value != "100")
       cerr << "settings max_rounds value: " << value << " not expected\n";
-  }
-  if (field == "your_botid") {
+  } else if (field == "your_botid") {
     if (value == "0" or value == "1")
       botId = value[0];
     else
@@ -110,12 +104,12 @@ void processSettings()
     cerr << "settings field: " << field << " not expected\n";
 }
 
-void parseState(const string value)
+void parseState(const string &value)
 {
   int row = 0;
   int col = 0;
   for (const char& c : value) {
-    if (c != '.') {
+    if (c != ',') {
       state[row][col] = c;
       if (col == 17) {
         col = 0;
@@ -130,12 +124,13 @@ void parseState(const string value)
 void makeMove()
 {
   int numMoves = 1 + (myLiveCells + theirLiveCells) + (6 * (288 - myLiveCells - theirLiveCells));
+  node bestKillNodes[4];
   node nodes[numMoves];
+
   addPassNodes(nodes);
   addKillNodes(nodes);
-  int bestKillMoveIdxs[4];
-  findBestKillNodes(nodes, bestKillMoveIdxs);
-  addBirthNodes(nodes, bestKillMoveIdxs);
+  findBestKillNodes(nodes, bestKillNodes);
+  addBirthNodes(nodes, bestKillNodes);
   // TODO: find best move, make move, api
 }
 
@@ -152,11 +147,12 @@ void addPassNodes(node nodes[])
 void addKillNodes(node nodes[])
 {
   int i = 0;
-  for (int c = 0; c < 19; c++) {
-    for (int r = 0; r < 17; r++) {
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
       if (state[r][c] != '.') {
         node n;
         n.type = 'k';
+        n.target = r * 18 + c;
         copyState(n.state);
         n.state[r][c] = '.';
         calculateNextState(n);
@@ -167,145 +163,172 @@ void addKillNodes(node nodes[])
   }
 }
 
-void findBestKillNodes(const node nodes[], int bestKillMoves[])
+void findBestKillNodes(const node nodes[], node bestKillNodes[])
 {
-  int bestNode1[2];
-  bestNode1[0] = 0;
-  bestNode1[1] = 0;
-  int bestNode2[2];
-  bestNode2[0] = 0;
-  bestNode2[1] = 0;
-  int bestNode3[2];
-  bestNode3[0] = 0;
-  bestNode3[1] = 0;
-  int bestNode4[2];
-  bestNode4[0] = 0;
-  bestNode4[1] = 0;
+  node bestNode1 = nodes[1];
+  node bestNode2 = nodes[1];
+  node bestNode3 = nodes[1];
+  node bestNode4 = nodes[1];
 
-  for (int i = 0; i < 288; i++) {
-    node n  = nodes[i];
-    if (n.type == 'k' and n.heuristicValue > bestNode4[0]) {
-      if (n.heuristicValue > bestNode3[0]) {
-        bestNode4[0] = bestNode3[0];
-        bestNode4[1] = bestNode3[1];
-        if (n.heuristicValue > bestNode2[0]) {
-        bestNode3[0] = bestNode2[0];
-        bestNode3[1] = bestNode2[1];
-          if (n.heuristicValue > bestNode1[0]) {
-            bestNode2[0] = bestNode1[0];
-            bestNode2[1] = bestNode1[1];
-            bestNode1[0] = n.heuristicValue;
-            bestNode1[1] = i;
+  if (false) {
+    int i = 1;
+    node n = nodes[i];
+    if (n.heuristicValue > bestNode4.heuristicValue) {
+      if (n.heuristicValue > bestNode3.heuristicValue) {
+        bestNode4 = bestNode3;
+        if (n.heuristicValue > bestNode2.heuristicValue) {
+        bestNode3 = bestNode2;
+          if (n.heuristicValue > bestNode1.heuristicValue) {
+            bestNode2 = bestNode1;
+            bestNode1 = n;
           } else {
-            bestNode2[0] = n.heuristicValue;
-            bestNode2[1] = i;
+            bestNode2 = n;
           }
         } else {
-          bestNode3[0] = n.heuristicValue;
-          bestNode3[1] = i;
+          bestNode3 = n;
         }
       } else {
-        bestNode4[0] = n.heuristicValue;
-        bestNode4[1] = i;
+        bestNode4 = n;
+      }
+    }
+  }
+
+  bestKillNodes[0] = bestNode1;
+  bestKillNodes[1] = bestNode2;
+  bestKillNodes[2] = bestNode3;
+  bestKillNodes[3] = bestNode4;
+}
+
+void addBirthNodes(node nodes[], const node bestKillNodes[])
+{
+  int i = myLiveCells + theirLiveCells;
+  for (int x = 0; x < 3; x++) {
+    for (int y = x + 1; y < 4; y++) {
+      for (int c = 0; c < 18; c++) {
+        for (int r = 0; r < 16; r++) {
+          if (state[r][c] == '.') {
+            node n;
+            n.type = 'b';
+            n.target = r * 18 + c;
+            n.sacrifice1 = bestKillNodes[x].target;
+            n.sacrifice2 = bestKillNodes[y].target;
+            copyState(n.state);
+            n.state[r][c] = botId;
+            n.state[n.sacrifice1 / 18][n.sacrifice1 % 18] = '.';
+            n.state[n.sacrifice2 / 18][n.sacrifice2 % 18] = '.';
+            calculateNextState(n);
+            calculateHeuristic(n);
+            nodes[++i] = n;
+          }
+        }
       }
     }
   }
 }
 
-void addBirthNodes(node nodes[], const int bestKillMoves[])
-{
-}
-
 void copyState(char target[][18])
 {
-  for (int c = 0; c < 19; c++) {
-    for (int r = 0; r < 17; r++) {
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
       target[r][c] = state[r][c];
     }
   }
 }
 
-void calculateNextState(node n)
+void calculateNextState(node &n)
 {
   int neighbours0[16][18];
   int neighbours1[16][18];
 
-  for (int c = 0; c < 19; c++) {
-    for (int r = 0; r < 17; r++) {
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
+      neighbours0[r][c] = 0;
+      neighbours1[r][c] = 0;
+    }
+  }
+
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
       if (n.state[r][c] == '0') {
         if (c > 0) {
           neighbours0[r][c - 1]++;
-          if (r > 0)
-            neighbours0[r - 1][c - 1]++;
-          if (r < 15)
-            neighbours0[r + 1][c - 1]++;
+          if (r > 0) neighbours0[r - 1][c - 1]++;
+          if (r < 15) neighbours0[r + 1][c - 1]++;
         }
         if (c < 17) {
           neighbours0[r][c + 1]++;
-          if (r > 0)
-            neighbours0[r - 1][c + 1]++;
-          if (r < 15)
-            neighbours0[r + 1][c + 1]++;
+          if (r > 0) neighbours0[r - 1][c + 1]++;
+          if (r < 15) neighbours0[r + 1][c + 1]++;
         }
-        if (r > 0)
-          neighbours0[r - 1][c]++;
-        if (r < 15)
-          neighbours0[r + 1][c]++;
+        if (r > 0) neighbours0[r - 1][c]++;
+        if (r < 15) neighbours0[r + 1][c]++;
       }
       if (n.state[r][c] == '1') {
         if (c > 0) {
           neighbours1[r][c - 1]++;
-          if (r > 0)
-            neighbours1[r - 1][c - 1]++;
-          if (r < 15)
-            neighbours1[r + 1][c - 1]++;
+          if (r > 0) neighbours1[r - 1][c - 1]++;
+          if (r < 15) neighbours1[r + 1][c - 1]++;
         }
         if (c < 17) {
           neighbours1[r][c + 1]++;
-          if (r > 0)
-            neighbours1[r - 1][c + 1]++;
-          if (r < 15)
-            neighbours1[r + 1][c + 1]++;
+          if (r > 0) neighbours1[r - 1][c + 1]++;
+          if (r < 15) neighbours1[r + 1][c + 1]++;
         }
-        if (r > 0)
-          neighbours1[r - 1][c]++;
-        if (r < 15)
-          neighbours1[r + 1][c]++;
+        if (r > 0) neighbours1[r - 1][c]++;
+        if (r < 15) neighbours1[r + 1][c]++;
       }
     }
   }
 
   int neighbours;
-  for (int c = 0; c < 19; c++) {
-    for (int r = 0; r < 17; r++) {
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
       neighbours = neighbours0[r][c] + neighbours1[r][c];
       if (n.state[r][c] == '.' and neighbours == 3) {
-        if (neighbours0[r][c] > neighbours1[r][c])
-          n.state[r][c] = '0';
-        else
-          n.state[r][c] = '1';
-      } else if (n.state[r][c] != '.' and (neighbours == 2 or neighbours == 3))
+        n.state[r][c] = (neighbours0[r][c] > neighbours1[r][c]) ?  '0' : '1';
+      } else if (n.state[r][c] != '.' and (neighbours < 2 or neighbours > 3)) {
         n.state[r][c] = '.';
+      }
     }
   }
 }
 
-void calculateHeuristic(node n)
+void calculateHeuristic(node &n)
 {
   int cellCount0 = 0;
   int cellCount1 = 0;
 
-  for (int c = 0; c < 19; c++) {
-    for (int r = 0; r < 17; r++) {
-      if (n.state[r][c] == '0')
+  for (int c = 0; c < 18; c++) {
+    for (int r = 0; r < 16; r++) {
+      if (n.state[r][c] == '0') {
         cellCount0++;
-      if (n.state[r][c] == '1')
+      } else if (n.state[r][c] == '1') {
         cellCount1++;
+      }
     }
   }
 
-  if (botId == '0')
-    n.heuristicValue = cellCount0 - cellCount1;
-  if (botId == '1')
-    n.heuristicValue = cellCount1 - cellCount0;
+  if (botId == '0') {
+    if (cellCount0 == 0) {
+      n.heuristicValue = 0;
+    } else if (cellCount1 == 0) {
+      n.heuristicValue = 288;
+    } else {
+      n.heuristicValue = cellCount0 - cellCount1;
+    }
+  } else if (botId == '1') {
+    if (cellCount1 == 0) {
+      n.heuristicValue = 0;
+    } else if (cellCount0 == 0) {
+      n.heuristicValue = 288;
+    } else {
+      n.heuristicValue = cellCount1 - cellCount0;
+    }
+  }
+}
+
+/* DEBUG FUNCTIONS */
+void setBotId(const char id)
+{
+  botId = id;
 }
--- miscellany/riddle-competitions/golad/c++/game.h
+++ miscellany/riddle-competitions/golad/c++/game.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -11,20 +12,24 @@ struct node
   char state[16][18];
   int heuristicValue;
   char type;
+  int target;
+  int sacrifice1;
+  int sacrifice2;
 };
 
 void game();
 void processAction();
 void processUpdate();
 void processSettings();
-void parseState(const string value);
+void parseState(const string &value);
 void makeMove();
 void addPassNodes(node nodes[]);
 void addKillNodes(node nodes[]);
-void findBestKillNodes(const node nodes[], int bestKillMoves[]);
-void addBirthNodes(node nodes[], const int bestKillMoves[]);
+void findBestKillNodes(const node nodes[], node bestKillNodes[]);
+void addBirthNodes(node nodes[], const node bestKillNodes[]);
 void copyState(char target[][18]);
-void calculateNextState(node n);
-void calculateHeuristic(node n);
+void calculateNextState(node &n);
+void calculateHeuristic(node &n);
+void setBotId(const char id);
 
 #endif
