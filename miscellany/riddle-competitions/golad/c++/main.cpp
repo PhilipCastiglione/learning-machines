612991a83eaea0fd9e028c63e9ff3f7d6dfb0f59
Philip Castiglione <philipcastiglione@gmail.com>
2018-01-29 22:59:41 +1100

    added TODOs

--- miscellany/riddle-competitions/golad/c++/main.cpp
+++ miscellany/riddle-competitions/golad/c++/main.cpp
@@ -1,9 +1,14 @@
 // TODO: author, licence, README
-// TODO: handle less than 4 kill targets for sacrifice
+// TODO: refactor
+// TODO: make logging dependent on execution parameter
+// TODO: fix bugs
+// TODO: decide parameters
+// TODO: handle less than n kill targets for sacrifice
 // TODO: use time, get smarter
-// TODO: define omptisation variables as constants
-// TODO: choose best n moves to calculate opponents moves for
+// TODO: test suite
 // TODO: improve heuristic
+// TODO: use array templates for 2d array
+// TODO: refactor multi-dim array to ignore/singledim array for SPEEEEEEEED
 #include "test.h"
 #include "game.h"
 
