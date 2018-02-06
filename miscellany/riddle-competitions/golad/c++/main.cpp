9716523c745e6edce8922e32fe694669bf40505e
Philip Castiglione <philipcastiglione@gmail.com>
2018-02-06 22:00:15 +1100

    updated main, removed redundant "game" files

--- miscellany/riddle-competitions/golad/c++/main.cpp
+++ miscellany/riddle-competitions/golad/c++/main.cpp
@@ -1,23 +1,15 @@
 // TODO: author, licence, README
-// TODO: refactor and separate responsibilities
+// TODO: test, test suite
 // TODO: fix bugs
 // TODO: optimise parameters
-// TODO: handle less than n kill targets for sacrifice
+// TODO: handle less than n kill targets for sacrifice (?)
 // TODO: use time, get smarter
-// TODO: improve test suite
 // TODO: improve heuristic
-// TODO: use array templates for 2d array
-// TODO: refactor multi-dim array to ignore/singledim array for SPEEEEEEEED
-//#include "test.h"
-#include "game.h"
+#include "skynet.h"
 
 int main(int argc, char *argv[])
 {
- // if (argc == 1) {
-    game();
-  //} else {
-   // test();
-  //}
+  crushEnemies();
 
   return 0;
 }
